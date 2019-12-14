9a718ff9f8b9cdc552ac9ace73502cc4f09bc9c8
felipecordente <felipe_1998_@outlook.com>
2019-12-14 21:25:27 +0100

    añadir src y target

---
+++ src/main/java/urjc/isi/prueba/App.java
@@ -0,0 +1,13 @@
+package urjc.isi.prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
